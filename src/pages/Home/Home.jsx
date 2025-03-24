8a439fe1ffe2496b751e4aff3645bde1949bae66
Ashish Tripathi <ashishtripathi0414@gmail.com>
2025-03-24 21:29:49 +0530

    Changes in Home.jsx for Footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import hero_title from '../../assets/hero_title.png';
 import play_icon from '../../assets/play_icon.png';
 import info_icon from '../../assets/info_icon.png';
 import TitleCards from '../../components/TitleCards/Titlecards.jsx';
-import Footer from '../../components/footer/Footer.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
 const Home = () => {
     return (
         <div className='home'>
